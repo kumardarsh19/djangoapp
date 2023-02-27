d00165ecacadb0421099cbb1d2a10e3397f7ed20
Alejandro Ruiz <aruiz2@andrew.cmu.edu>
2023-02-27 13:57:37 -0500

    define height parameter for find_peaks

--- rhythm_processor/rhythm.py
+++ rhythm_processor/rhythm.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 found = 1
-#time_interval is a sixteenth note
+not_found = 0
+onset_height = 1000
 
 def plot(left_bound, right_bound, y, peak_pos, height):
     x = np.linspace(left_bound, right_bound, len(y))
@@ -28,23 +29,21 @@ def rhythm(audio_file):
     samples = len(audio_data)
     audio_length = samples // sampling_rate
     peaks = []
+    print(audio_data)
     print(f"\n\taudio_length: {audio_length}\n\tsamples: {samples}\n\tsampling_rate: {sampling_rate}\n\ttime_interval: {time_interval}")
-    #plot_time(audio_data, audio_length, samples)
+    plot_time(audio_data, audio_length, samples)
 
     for i in np.arange(0, samples, time_interval):
         left_bound = i
         right_bound = i + time_interval
         signal = audio_data[left_bound:right_bound]
-        localPeaksX, localPeaksInfo = find_peaks(signal, distance=time_interval)
-        # print(localPeaksX)
-        # print(localPeaksInfo)
-        # print()
+        localPeaksX, localPeaksInfo = find_peaks(signal, distance=time_interval, height=onset_height)
 
         # Append a 1 if there is a peak, 0 otherwise.
         if len(localPeaksX) > 0:
             peaks.append(found)
         else:
-            peaks.append(not found)
+            peaks.append(not_found)
     print(f"peaks: {peaks}")
     return peaks
 
