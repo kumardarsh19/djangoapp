1b97ef0ddc189991b1398dde822b2299130aa6a1
Alejandro Ruiz <aruiz2@andrew.cmu.edu>
2023-03-13 11:40:58 -0400

    created the audioForm

--- pitchapp/forms.py
+++ pitchapp/forms.py
@@ -1,7 +1,24 @@
 from django import forms;
-from pitchprocessor import getNoteList;
+from pitchprocessor import getPitchList;
 
 class audioForm(forms.Form):
-    file = forms.FileField();
-
+    class Meta:
+        fields = ['file', 'clef', 'time signature']
+        widgets = {
+            'file': forms.FileInput(attrs={'class': 'form-control'}),
+            'clef': forms.Select(attrs={'class': 'form-control'}),
+            'time signature': forms.Select(attrs={'class': 'form-control'})
+        }
+        labels = {
+            'file': 'Upload an audio file',
+            'clef': 'Select a clef',
+            'time signature': 'Select a time signature'
+        }
     
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+        if file is None:
+            raise forms.ValidationError("You did not upload a file.")
+        if file.name.endswith('.wav') is False:
+            raise forms.ValidationError("You did not upload a .wav file.")
+        return file
