ce5e8557181ee73b411083e95665b4748d699764
Alejandro Ruiz <aruiz2@andrew.cmu.edu>
2023-03-18 14:45:28 -0400

    made changes to rhytm processor so that it is able to handle audio files with multiple channels and not only one channel

--- audioprocessing/rhythmprocessor.py
+++ audioprocessing/rhythmprocessor.py
@@ -3,7 +3,7 @@ from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from audioprocessing.preprocessing import normalize;
+from preprocessing import normalize;
 
 found = 1
 not_found = 0
@@ -33,14 +33,11 @@ def detect_beats_channels(audio_data) -> np.array:
             data_length = len(audio_data)
             channel_length = len(audio_data[0])
             for data in range(data_length):
-                beat_found = False
+                greatest_beat = 0
                 for channel in range(channel_length):
-                    if audio_data[data][channel] == 1:
-                        beat_found = True
-                if beat_found:
-                    beats.append(1)
-                else:
-                    beats.append(0)
+                    if np.abs(audio_data[data][channel]) > greatest_beat:
+                        greatest_beat = np.abs(audio_data[data][channel])
+                beats.append(greatest_beat)
             print("New beats\n")
             return np.array(beats)
     except:
@@ -51,16 +48,10 @@ def getOnsetList(audio_file):
     #TODO: If wavfile.read returns 2D array, this code will not work.
     sampling_rate, audio_data = wavfile.read("audios/C-scale.wav")
     audio_data = detect_beats_channels(audio_data)
-    audio_data = normalize(audio_data)
-    max_amplitude = np.max(np.abs(audio_data))
     time_interval = sampling_rate // 8
     samples = len(audio_data)
     audio_length = samples // sampling_rate
     peaks = []
-
-    #print(audio_data)
-    #print(f"\n\taudio_length: {audio_length}\n\tsamples: {samples}\n\tsampling_rate: {sampling_rate}\n\ttime_interval: {time_interval}")
-    #plot_time(audio_data, audio_length, samples)
     start_time = time.time()
     for i in np.arange(0, samples, time_interval):
         left_bound = i
@@ -69,17 +60,10 @@ def getOnsetList(audio_file):
         localPeaksX, localPeaksInfo = find_peaks(signal, 
                                                  distance=time_interval, 
                                                  height=onset_height)
-
-        # Append a 1 if there is a peak, 0 otherwise.
         if len(localPeaksX) > 0:
             peaks.append(found)
         else:
             peaks.append(not_found)
     print(f"\n{peaks}")
     print("\n\n--- %s seconds ---" % (time.time() - start_time))
-
     return peaks
-
-if __name__ == "__main__":
-    audio_file = "audio_files/C scale.m4a"
-    getOnsetList(audio_file)
