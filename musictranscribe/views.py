cdc81f77da17349dcb1b74ec2685e825eeb12cf3
Alejandro Ruiz <aruiz2@andrew.cmu.edu>
2023-03-22 11:14:43 -0400

    able to draw clef and time signature using vexflow based on user input

--- musictranscribe/views.py
+++ musictranscribe/views.py
@@ -4,20 +4,16 @@ from audioprocessing.rhythmprocessor import getOnsetList
 from audioprocessing.integrator import getNoteList
 from .forms import AudioForm
 
-# Create your views here.
-
 def home_view(request):
     context = {'form': AudioForm()}
     if request.method == 'POST':
         form = AudioForm(request.POST, request.FILES)
         if form.is_valid():
-            clef = form.cleaned_data['clef']
-            time_signature = form.cleaned_data['time_signature']
             file = form.cleaned_data['file']
-            print(f"file: {file}, clef: {clef}, time_signature: {time_signature}")
-
-            notesPitches = getPitchList(file)
-            notesOnsets = getOnsetList(file)
-            context['notes'] = getNoteList(notesPitches, notesOnsets, clef)
+            print(f"file: {file}, clef: {form.cleaned_data['clef']}, time_signature: {form.cleaned_data['time_signature']}")
+            context['pitches'] = getPitchList(file)
+            context['onsets'] = getOnsetList(file)
+            context['clef'] = form.cleaned_data['clef']
+            context['timeSignature'] = form.cleaned_data['time_signature']
             return render(request, 'home.html', context)
     return render(request, "home.html", context)
