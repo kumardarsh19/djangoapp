74e24331a787be2dfd69222b2d196e561c5ab758
Alejandro Ruiz <aruiz2@andrew.cmu.edu>
2023-03-22 13:58:03 -0400

    move getNotes integrator function to separate file, with Note class too

---
+++ musictranscribe/static/js/pdf.js
@@ -0,0 +1,20 @@
+function writeNotes(notelist) {
+
+    let notes = [];
+
+    for (let i=0; i<notelist.length; i++) {
+        curr = new StaveNote({keys: [notelist[i]], duration: "q"});
+        notes[i] = curr;
+    }
+
+    // Helper function to justify and draw a 4/4 voice.
+    Formatter.FormatAndDraw(context, stave, notes);
+
+    const doc = new jsPDF();
+    const svgElement = div.childNodes[0];
+    doc.svg(svgElement).then(() => doc.save("score.pdf"));
+
+    console.log("Saved score.pdf");
+}
+
+
--- musictranscribe/static/js/transcribe.js
+++ musictranscribe/static/js/transcribe.js
@@ -1,23 +1,41 @@
-function writeNotes(notelist) {
-
-    let notes = [];
-
-    for (let i=0; i<notelist.length; i++) {
-        curr = new StaveNote({keys: [notelist[i]], duration: "q"});
-        notes[i] = curr;
+// Note class for Vex.Flow
+export class Note {
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
     }
-
-    // Helper function to justify and draw a 4/4 voice.
-    Formatter.FormatAndDraw(context, stave, notes);
-
-    const doc = new jsPDF();
-    const svgElement = div.childNodes[0];
-    doc.svg(svgElement).then(() => doc.save("score.pdf"));
-
-    console.log("Saved score.pdf");
-
-   
-
 }
 
-
+// getNoteList integrates the output of rhythm and pitch processor
+// into a list of notes that can be used by Vex.Flow.
+export function getNoteList(pitches , onsets) {
+    let notes = [];
+    let length = pitches.length;
+    let duration = 1;
+    for (let i = 1; i < length; i++) {
+        if (pitches[i] == pitches[i-1]) {
+            // print("increment duration")
+            duration ++;
+        } else if (pitches[i] != pitches[i-1] && onsets[i-1] == 1) {
+            // Incorporate a regular note
+            // print("regular")
+            let note = new Note(pitches[i-1], duration/8);
+            notes.push(note);
+            duration = 1;
+        } else {
+            // Incorporate a rest note
+            // print("rest")
+            let note = new Note('##', duration/8);
+            notes.push(note);
+        }
+    }
+    // Add the last note.
+    if (onsets[length-1] == 1) {
+        let note = new Note(pitches[length-1], duration/8);
+        notes.push(note);
+    } else {
+        let note = new Note('##', duration/8);
+        notes.push(note);
+    }
+    return notes;
+}
