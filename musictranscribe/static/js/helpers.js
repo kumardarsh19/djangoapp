8e872373df5e4b63b06090814d11d8666335685d
Adi-Agarwal <45182847+Adi-Agarwal@users.noreply.github.com>
2023-03-27 11:23:34 -0400

    draw bars, messy

--- musictranscribe/static/js/helpers.js
+++ musictranscribe/static/js/helpers.js
@@ -1,11 +1,21 @@
 
 
 
-function getStaves(num) {
-    ret = [];
-
-    for (let i=0; i<num; i++) {
-        curr = new Stave()
+function getStaves(num, startStave) {
+    const {Stave} = Vex.Flow;
+    ret = [startStave];
+    startx = startStave.x;
+    starty = startStave.y;
+    width = startStave.width;
+    height = startStave.height;
+    cW = startStave.getContext().height;
+    cH = startStave.getContext().height;
+    for (let i=1; i<num; i++) {
+        prev = ret[i-1];
+        curr = new Stave((startx + i * width) % cW, (starty + i * height) % cH, width);
+        ret.push(curr);
     }
+
+    return ret;
     
 }
