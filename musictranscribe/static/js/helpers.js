fb9cf4d3c477c28b4bae330dcb5ca7ae837bc0fc
Adi-Agarwal <45182847+Adi-Agarwal@users.noreply.github.com>
2023-04-01 13:05:10 -0400

    alternate getNotes method

--- musictranscribe/static/js/helpers.js
+++ musictranscribe/static/js/helpers.js
@@ -18,3 +18,34 @@ export function getStaves(startx, starty, width, height, cw, ch, Vex) {
 export function getBaseLog(x, y) {
     return Math.log(y) / Math.log(x);
 }
+
+function isValidDuration(dur) {
+    return dur == 1 || dur == 2 || dur == 4 || dur == 8 || dur == 16;
+}
+export function getStaveNotes(pitches, onsets, Vex) {
+    const {Stave, StaveNote} = Vex.Flow;
+    var ret = [];
+    let startIndex = pitches.find(element => element != "##");
+    let currNote = pitches[startIndex];
+    let endIndex = pitches.slice(startIndex).find(element => element != currNote);
+    while (startIndex < pitches.length && endIndex < pitches.length
+           && startIndex != undefined && endIndex != undefined) {
+        let onsetSlice = onsets.slice(startIndex, onsets.find(element => element == 0));
+        let duration = Math.round(onsetSlice.length / 8);
+        while (!isValidDuration(duration)) duration++;
+        let keys = [currPitch];
+        if (keys[0] == '##') keys = [];
+        let types = ['r', 'n'];
+        if (keys.length == 0) type = 'r';
+        let vexNote = new StaveNote({
+            keys: keys,
+            duration: duration,
+            type: types[keys.length - 1],
+        })
+        ret.push(vexNote);
+        startIndex = endIndex;
+        endIndex = pitches.slice(startIndex).find(element => element != currNote);
+    }
+
+    return ret;
+}
