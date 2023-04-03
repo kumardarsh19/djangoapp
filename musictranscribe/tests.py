bf4d1e8787e7200358599b5a66399447d7a6a04e
Adi-Agarwal <45182847+Adi-Agarwal@users.noreply.github.com>
2023-04-03 16:14:57 -0400

    create test for splitNotes

--- musictranscribe/tests.py
+++ musictranscribe/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from musictranscribe.noteformatting import *
 
 # Create your tests here.
+
+class FormattingTestCase(TestCase):
+    def setUp(self):
+        self.fourQuarterNotes = [{
+            'key': 'C/4',
+            'duration': '4',
+            'typ': 'n'
+        }] * 4
+
+        self.fourHalfNotes = [{
+            'key': 'C/4',
+            'duration': '2',
+            'typ': 'n'
+        }] * 4
+
+    def testSplitNotes(self):
+        halfNotes = splitNotes(self.fourHalfNotes)
+        assert len(halfNotes) == 4, "Measure is wrong length"
+        for note in halfNotes:
+            assert note['duration'] == '2', "Duration is wrong"
